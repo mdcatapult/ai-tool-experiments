1ca0557425ce031e0fc80e6e24e4620939e66998
temitope femitemiola <temitope.femitemiola@md.catapult.org.uk>
2024-01-18 15:26:19 +0000

    provided classes for the task

--- src/erbium-app/erbium-ai.py
+++ src/erbium-app/erbium-ai.py
@@ -1,41 +1,61 @@
 from crewai import Agent, Task, Crew, Process
 import os
-from klein_config import get_config
+from open_ai_config.openai_config import OPENAI_API_KEY
 
-config = get_config()
-
-
-os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY")
-
-researcher = Agent(
-    role="Researcher",
-    goal="Research new AI insights",
-    backstory="You are a researcher at a university. You are working on a new AI algorithm that will help people with their daily lives.",
-    verbose=True,
-    allow_delegation=False,
-)
+# import the OpenAI API key from the environment
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
-writer = Agent(
-    role="Writer",
-    goal="Write a paper about the new AI algorithm",
-    backstory="You are a writer at a university. You are writing a paper about a new AI algorithm that will help people with their daily lives.",
-    verbose=True,
-    allow_delegation=False,
-)
+class AgentRoles:
+    def __init__(self):
+        self.agent = Agent(**self.kwargs)
+
+    researcher = Agent(
+        role="Researcher",
+        goal="Research new AI insights",
+        backstory="You are a researcher at a university. You are working on a new AI algorithm that will help people with their daily lives.",
+        verbose=True,
+        allow_delegation=False,
+    )
+
+    writer = Agent(
+        role="Writer",
+        goal="Write a paper about the new AI algorithm",
+        backstory="You are a writer at a university. You are writing a paper about a new AI algorithm that will help people with their daily lives.",
+        verbose=True,
+        allow_delegation=False,
+    )
 
 
-task1 = Task(description="Investigate the latest medical research", agent=researcher)
-task2 = Task(description="Investigate the latest AI research", agent=writer)
+class Tasks:
+    def __init__(self):
+        self.task = Task(**self.kwargs)
 
-crew = Crew(
-    name="AI Research Crew",
-    agents=[researcher, writer],
-    tasks=[task1, task2],
-    verbose=2,
-    process=Process.sequential,
-)
+    task1 = Task(
+        description="Investigate the latest medical research",
+        agent=AgentRoles.researcher,
+    )
 
-result = crew.kickoff()
+    task2 = Task(
+        description="Investigate the latest AI research", agent=AgentRoles.writer
+    )
 
-print(result)
+
+class CrewAI:
+    def __init__(self):
+        self.crew = Crew(
+            name="AI Research Crew",
+            agents=[AgentRoles.researcher, AgentRoles.writer],
+            tasks=[Tasks.task1, Tasks.task2],
+            verbose=2,
+            process=Process.sequential,
+        )
+
+    def kickoff(self):
+        return self.crew.kickoff()
+
+
+if __name__ == "__main__":
+    crew = CrewAI()
+    result = crew.kickoff()
+    print(result)
