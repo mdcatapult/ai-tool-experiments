7aafea5dfa487b612a6dee869cfe9b7bf4bf84bd
temitope femitemiola <temitope.femitemiola@md.catapult.org.uk>
2024-01-19 09:49:00 +0000

    defined the task, agents in a class

--- src/erbium-app/erbium-ai.py
+++ src/erbium-app/erbium-ai.py
@@ -6,47 +6,45 @@ from open_ai_config.openai_config import OPENAI_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
+# Agent roles are defined and instantiated here
 class AgentRoles:
     def __init__(self):
-        self.agent = Agent(**self.kwargs)
+        self.researcher = Agent(
+            role="Researcher",
+            goal="Research new AI insights",
+            backstory="You are a researcher at a university. You are working on a new AI algorithm that will help people with their daily lives.",
+            verbose=True,
+            allow_delegation=False,
+        )
 
-    researcher = Agent(
-        role="Researcher",
-        goal="Research new AI insights",
-        backstory="You are a researcher at a university. You are working on a new AI algorithm that will help people with their daily lives.",
-        verbose=True,
-        allow_delegation=False,
-    )
-
-    writer = Agent(
-        role="Writer",
-        goal="Write a paper about the new AI algorithm",
-        backstory="You are a writer at a university. You are writing a paper about a new AI algorithm that will help people with their daily lives.",
-        verbose=True,
-        allow_delegation=False,
-    )
+        self.writer = Agent(
+            role="Writer",
+            goal="Write a paper about the new AI algorithm",
+            backstory="You are a writer at a university. You are writing a paper about a new AI algorithm that will help people with their daily lives.",
+            verbose=True,
+            allow_delegation=False,
+        )
 
 
+# Tasks are defined here
 class Tasks:
     def __init__(self):
-        self.task = Task(**self.kwargs)
+        self.task1 = Task(
+            description="Investigate the latest medical research",
+            agent=AgentRoles().researcher,
+        )
 
-    task1 = Task(
-        description="Investigate the latest medical research",
-        agent=AgentRoles.researcher,
-    )
-
-    task2 = Task(
-        description="Investigate the latest AI research", agent=AgentRoles.writer
-    )
+        self.task2 = Task(
+            description="Investigate the latest AI research", agent=AgentRoles().writer
+        )
 
 
 class CrewAI:
     def __init__(self):
         self.crew = Crew(
             name="AI Research Crew",
-            agents=[AgentRoles.researcher, AgentRoles.writer],
-            tasks=[Tasks.task1, Tasks.task2],
+            agents=[AgentRoles().researcher, AgentRoles().writer],
+            tasks=[Tasks().task1, Tasks().task2],
             verbose=2,
             process=Process.sequential,
         )
