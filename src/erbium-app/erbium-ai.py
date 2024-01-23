5598863a844cb72351dc2de02f4ff952df7c657e
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2024-01-23 16:33:07 +0000

    Try a medicines focussed agent

--- src/erbium-app/erbium-ai.py
+++ src/erbium-app/erbium-ai.py
@@ -1,14 +1,14 @@
 from crewai import Agent, Task, Crew, Process
 from langchain_community.llms import Ollama
 from langchain_community.tools import DuckDuckGoSearchRun
-from open_ai_config.openai_config import OPENAI_API_KEY
+from src.open_ai_config.openai_config import OPENAI_API_KEY
 import os
 
 # import the OpenAI API key from the os environment
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 # instantiate the Ollama language model , you can use different models
-ollama_llm = Ollama(model="llama2")
+ollama_llm = Ollama(model="llama2", verbose=True, temperature=0.6)
 
 # using the DuckDuckGoSearchRun tool
 search_tool = DuckDuckGoSearchRun()
@@ -18,9 +18,11 @@ search_tool = DuckDuckGoSearchRun()
 class AgentRoles:
     def __init__(self):
         self.researcher = Agent(
-            role="Researcher",
-            goal="Research new AI insights",
-            backstory="You are a researcher at a university. You are working on a new AI algorithm that will help people with their daily lives.",
+            role="Senior Drug Discovery scientist",
+            goal="Uncover cutting edge advancements in medicines discovery technology and drugs",
+            backstory="""You work at a major pharmaceutical drug discovery company.
+            Your expertise lies in identifying how existing and new technologies
+            can be applied in the field of medicines discovery.""",
             verbose=True,
             allow_delegation=False,
             tools=[search_tool],
@@ -29,8 +31,10 @@ class AgentRoles:
 
         self.writer = Agent(
             role="Writer",
-            goal="Write a paper about the new AI algorithm",
-            backstory="You are a writer at a university. You are writing a paper about a new AI algorithm that will help people with their daily lives.",
+            goal="Write compelling and succinct summaries of the latest advancements in medicines discovery.",
+            backstory="""You are a world famous writer with a passion for science and technology.
+            You have been asked to write a blog post on the latest advancements in medicines discovery. 
+            You translate complex scientific concepts into easy to understand language.""",
             verbose=True,
             allow_delegation=False,
             llm=ollama_llm,
@@ -41,12 +45,16 @@ class AgentRoles:
 class Tasks:
     def __init__(self):
         self.task1 = Task(
-            description="Investigate the latest medical research",
-            agent=AgentRoles().researcher,
+            description="""Conduct a comprehensive review of the latest medical research over the last 2 months. Identify
+            breakthrough technologies and new drugs that are being developed.""",
+            agent=AgentRoles().researcher
         )
 
         self.task2 = Task(
-            description="Investigate the latest AI research", agent=AgentRoles().writer
+            description="""Using any insights provided, develop an engaging blog post style summary that
+            highlights current advancements in the field of medicines discovery. Avoid words that are too complex and provide layman
+            descriptions of any new technologies involved. Your final answer should contain at least 1 paragraph per technology.""",
+            agent=AgentRoles().writer
         )
 
 
@@ -59,7 +67,7 @@ class CrewAI:
             agents=[AgentRoles().researcher, AgentRoles().writer],
             tasks=[Tasks().task1, Tasks().task2],
             verbose=2,
-            process=Process.sequential,
+            # process=Process.sequential,
         )
 
     def start_simulation(self):
