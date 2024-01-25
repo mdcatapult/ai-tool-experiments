fc90c0431f89c09473040530af787c531fef6ab6
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2024-01-25 15:36:18 +0000

    Ingest word docs

--- src/app/llama-index.py
+++ src/app/llama-index.py
@@ -1,22 +1,51 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, download_loader, load_index_from_storage, StorageContext
+from pathlib import Path
 import os
 from src.open_ai_config.openai_config import OPENAI_API_KEY, DATA_IMPORT_DIRECTORY, DATA_PERSIST_DIRECTORY
 
-# export the OpenAI API key
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-if not os.path.exists(DATA_PERSIST_DIRECTORY):
-    # load the documents and create the index
-    documents = SimpleDirectoryReader(DATA_IMPORT_DIRECTORY).load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    # store it for later
-    index.storage_context.persist(persist_dir=DATA_PERSIST_DIRECTORY)
-else:
-    # load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=DATA_PERSIST_DIRECTORY)
-    index = load_index_from_storage(storage_context)
+class LLamaTest:
 
-query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
-print(response)
+    def start(self):
 
+        # export the OpenAI API key
+        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+        DocxReader = download_loader("DocxReader")
+        docx_reader = DocxReader()
+
+        if not os.path.exists(DATA_PERSIST_DIRECTORY):
+            # load the documents and create the index
+            # documents = SimpleDirectoryReader(DATA_IMPORT_DIRECTORY).load_data()
+            documents = []
+            # store it for later
+            for doc in self.get_docx_filepaths(DATA_IMPORT_DIRECTORY):
+                try:
+                    word_document = docx_reader.load_data(file=doc)
+                    documents = documents + word_document
+                except:
+                    print("Error loading document")
+            index = VectorStoreIndex.from_documents(documents)
+            index.storage_context.persist(persist_dir=DATA_PERSIST_DIRECTORY)
+        else:
+            # load the existing index
+            storage_context = StorageContext.from_defaults(persist_dir=DATA_PERSIST_DIRECTORY)
+            index = load_index_from_storage(storage_context)
+        query_engine = index.as_query_engine()
+        response = query_engine.query("What is the Yttrium project about?")
+        return response
+
+    def get_docx_filepaths(self, directory_path):
+        filepaths = []
+
+        for filename in os.listdir(directory_path):
+            if filename.endswith(".docx"):
+                file_path = os.path.join(directory_path, filename)
+                filepaths.append(Path(file_path))
+
+        return filepaths
+
+if __name__ == "__main__":
+    indexer = LLamaTest()
+    result = indexer.start()
+    print(result)
